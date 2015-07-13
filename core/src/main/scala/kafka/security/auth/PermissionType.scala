1ea29075c960bc1f3cb5abddb0bb6a46116a55de
Parth Brahmbhatt <brahmbhatt.parth@gmail.com>
2015-07-13 12:47:41 -0700

    Moving PermissionType to trait instead of enum. Following the convention for defining constants.

--- core/src/main/scala/kafka/security/auth/PermissionType.scala
+++ core/src/main/scala/kafka/security/auth/PermissionType.scala
@@ -5,18 +5,46 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kafka.security.auth;
+package kafka.security.auth
 
-public enum PermissionType {
-   ALLOW,
-   DENY
+;
+
+/**
+ * PermissionType.
+ */
+
+
+sealed trait PermissionType {
+  def name: String
 }
+
+case object Allow extends PermissionType {
+  val name: String = "Allow"
+}
+
+case object Deny extends PermissionType {
+  val name: String = "Deny"
+}
+
+object PermissionType {
+  def fromString(permissionType: String) : PermissionType = {
+    return permissionType match {
+      case permissionType if permissionType.equalsIgnoreCase(Allow.name) => Allow
+      case permissionType if permissionType.equalsIgnoreCase(Deny.name) => Deny
+    }
+  }
+
+  def values() : List[PermissionType] = {
+    return List(Allow, Deny)
+  }
+}
+
